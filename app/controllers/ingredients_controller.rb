15caee1ec03e79ddbf2150b18c052e7abfcaad24
Elliot Tennison <elliot.s.tennison@gmail.com>
2020-03-16 04:21:22 +0800

    added new/create and show pages for meals

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,32 @@
+class IngredientsController < ApplicationController
+  def new
+    @meal = Meal.find(params[:meal_id])
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @meal = Meal.find(params[:meal_id])
+    @ingredient = Ingredient.new(ingredient_params)
+    @ingredient.meal_id = @meal.id
+
+    if @ingredient.save
+      redirect_to @meal, notice: 'Ingredient was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    @meal = @ingredient.meal
+    @ingredient.destroy
+    redirect_to meal_path(@meal)
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:dose).permit(:name)
+  end
+
+end
