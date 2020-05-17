fe83b73ac3e71f382f2a91580e1ed2ae55049006
Elliot Tennison <elliot.s.tennison@gmail.com>
2020-05-17 14:52:44 +0100

    fixed sentence bug on quick-results page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,18 @@ class PagesController < ApplicationController
     emissions_per_kg = @food.emission_rate
     @one_year_emissions = params[:diet]["amount"].to_f * emissions_per_kg.to_f * @food.average_portion_size.to_f
 
+    if params["diet"]["amount"] == "12"
+      @amount = "once a month"
+    elsif params["diet"]["amount"] == "52"
+      @amount = "once a week"
+    elsif params["diet"]["amount"] == "156"
+      @amount = "3 times a week"
+    elsif params["diet"]["amount"] == "365"
+      @amount = "once a day"
+    elsif params["diet"]["amount"] == "1095"
+      @amount = "in every meal"
+    end
+
     car = Transport.where(name: 'Car')[0]
     train = Transport.where(name: 'Train')[0]
     domestic_flight = Transport.where(name: 'Flight (Domestic)')[0]
@@ -15,7 +27,7 @@ class PagesController < ApplicationController
 
     @car_km = @one_year_emissions / car.emissions
     @train_km = @one_year_emissions / train.emissions
-    @domestic_flight_km = @one_year_emissions/ domestic_flight.emissions
+    @domestic_flight_km = @one_year_emissions / domestic_flight.emissions
     @short_flight_km = @one_year_emissions / short_flight.emissions
     @long_flight_km = @one_year_emissions / long_flight.emissions
   end
